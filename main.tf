08bcb5d150eedbbf859deed7e389f79fa8d0f9d1
sudarshan24002 <sudarshan@cybernerds.in>
2026-07-03 16:51:27 +0530

    Update main.tf
    2 nd comment

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "example" {
   ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
+  instance_type = "t3.large"
 
   tags = {
     Name = "prism-test-instance"
